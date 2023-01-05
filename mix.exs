431976007c74849cfab5e90cf3200c442190ec11
Steven Luu <me@luu.io>
2023-01-04 21:32:06 -0800

    Updated mix.exs to call `package()` explicitly
    Currently getting warnings when build:
    
    warning: variable "package" does not exist and is being expanded to "package()", please use parentheses to remove the ambiguity or change the variable name

--- mix.exs
+++ mix.exs
@@ -3,12 +3,12 @@ defmodule Latlong.Mixfile do
 
   def project do
     [app: :latlong,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: "Latitude, Longitude Parser",
-     package: package,
+     package: package(),
      deps: deps(),
 
      # Docs
